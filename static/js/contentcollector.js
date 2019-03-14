b1c27d148d1648de089bec3ce83a248047a88cbd
Leandro Vital <leandro.santos@ima.sp.gov.br>
2019-03-14 14:55:18 -0300

    add copy paste table

--- static/js/contentcollector.js
+++ static/js/contentcollector.js
@@ -11,90 +11,45 @@ exports.collectContentLineBreak = function (hook, context) {
 exports.collectContentLineText= function (hook, context) {
     var n = context.node;
     var txt = context.text;
-
     if (txt) {
+        txt = "";        
         while (n) {
             if (n.tagName == 'TD') {
-                var elementName = n.getAttribute("name");
-                if (elementName == 'tData') {
-                    txt = txt.replace(/\\/g, "|");
-                    txt = txt.replace(/"/g, "'");
-                    break;
-                } else if (elementName == 'delimCell') {
-                    txt = '","';
-                    break;
-                } else if (elementName == 'payload') {
-                    txt = "{\"payload\":[[\"";
-                    break;
-                } else if (elementName == 'bracketAndcomma') {
-                    txt = n.innerHTML;
-                    break;
+
+                var existing = localStorage.getItem('payload');
+                existing = existing ? existing.split('||') : [];
+                existing.push(n.innerText);
+                localStorage.setItem('payload', existing.toString());
+
+                if (!n.nextElementSibling) {
+                    var payload = localStorage.getItem('payload');
+                    payload = payload ? payload.split(',') : [];
+    
+                    var tblRows = payload;
+
+                    localStorage.removeItem('payload');
+                    localStorage.clear();
+
+                    var payload = [[]];
+
+                    payload = [tblRows];
+                
+                    tableObj = {
+                        "payload": payload,
+                        "tblId": 1,
+                        "tblClass": "data-tables",
+                        "trClass": "alst",
+                        "tdClass": "hide-el"
+                    }
+                
+                    txt = JSON.stringify(tableObj);
+
                 }
+                
             }
             n = n.parentNode;
         }
+        
     }
     return txt;
-};
-
-
-exports.collectContentPre = function(hook, context){
-
-    var tags = ['table', 'tbody', 'tr', 'td'];
-
-    var tname = context.tname;
-    var state = context.state;
-    var lineAttributes = state.lineAttributes
-    var tagIndex = _.indexOf(tags, tname);
-
-    if(tagIndex >= 0) {
-        if (tags[tagIndex] == 'table') {
-
-            localStorage.setItem('table', '');
-
-            console.log(tags[tagIndex]);
-        
-        }
-        if (tags[tagIndex] == 'tr') {
-
-
-            var table = localStorage.getItem('table');
-
-            if (table) {
-
-                var data = table.concat(tags[tagIndex]);
-
-                localStorage.setItem('table', data);
-            }
-
-            
-
-            console.log(tags[tagIndex]);
-        }
-        if (tags[tagIndex] == 'td') {
-
-            var table = localStorage.getItem('table');
-
-            if (table) {
-
-                var data = table.concat(tags[tagIndex]);
-
-                localStorage.setItem('table', data);
-            }
-
-            console.log(tags[tagIndex]);
-        }
-
-    }
-
-};
-
-exports.collectContentPost = function(hook, context){
-    var tname = context.tname;
-    var state = context.state;
-    var lineAttributes = state.lineAttributes;
-    var cc = context.cc;
-
-    // console.log(context);
-    
 };
